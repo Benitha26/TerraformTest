350356fe4db3718d343b29587d28e0726e4f6b70
benitha26 <benithachoudary1997@gmail.com>
2024-12-11 19:31:30 -0500

    added database server and sql database

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
+#App Service Plan
 resource "azurerm_app_service_plan" "asp" {
   name                = "asp-ben-wus-tst"
   location            = azurerm_resource_group.rg.location
@@ -15,6 +16,7 @@ resource "azurerm_app_service_plan" "asp" {
   }
 }
 
+#App Service
 resource "azurerm_app_service" "as" {
   name                = "ase-ben-wus-tst-001"
   location            = azurerm_resource_group.rg.location
@@ -34,5 +36,35 @@ resource "azurerm_app_service" "as" {
     name  = "Database"
     type  = "SQLServer"
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
+
+#Database Server - MySQL
+resource "azurerm_mssql_server" "sqldb_server" {
+  name                         = "dbs-ben-wus-tst"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "login name" //login name
+  administrator_login_password = "password" //login password
+}
+
+#Database
+resource "azurerm_mssql_database" "sqldb" {
+  name         = "db-ben-wus-tst"
+  server_id    = azurerm_mssql_server.sqldb_server.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
   }
 }
